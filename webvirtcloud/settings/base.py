4792d8e2b835bd97b0c017387efc2454db7d3d22
Retspen <anatoliy.guskov@gmail.com>
2022-10-29 20:10:24 +0300

    Added mysql driver

--- webvirtcloud/settings/base.py
+++ webvirtcloud/settings/base.py
@@ -2,6 +2,7 @@
 Django settings for webvirtcloud project.
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,11 +86,18 @@ WSGI_APPLICATION = 'webvirtcloud.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'CONN_MAX_AGE': 3600,
+        'NAME': 'hubgridcloud',
+        'USER': os.environ.get('DB_USER', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', 3306),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    },
 }
-
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
