67a2c0c2dd630cf4749abf8ee24c5ac91bf6e5bd
Retspen <anatoliy.guskov@gmail.com>
2022-11-12 14:18:32 +0200

    Added project API by uuid

--- api/v1/project/urls.py
+++ api/v1/project/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from project.views import ProjectDefaultAPI, ProjectListAPI
+from project.views import ProjectDefaultAPI, ProjectListAPI, ProjectDataAPI
 
 urlpatterns = [
     re_path(r"default/?$", ProjectDefaultAPI.as_view(), name="project_default_api"),
     re_path(r"$", ProjectListAPI.as_view(), name="project_list_api"),
+    re_path(r"(?P<uuid>[0-9a-f-]+)/?$", ProjectDataAPI.as_view(), name="project_data_api"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- project/views.py
+++ project/views.py
@@ -8,8 +8,14 @@ from .serializers import ProjectSerializer
 class ProjectDefaultAPI(APIView):
     class_serializer = ProjectSerializer
 
+    def get_object(self, uuid):
+        try:
+            return Project.objects.get(user=user, is_default=True, is_deleted=False)
+        except Snippet.DoesNotExist:
+            raise Http404
+
     def get(self, request, *args, **kwargs):
-        projects = Project.objects.get(user=request.user, is_default=True, is_deleted=False)
+        projects = self.get_object(user=request.user)
         serilizator = self.class_serializer(projects, many=False)
         return Response({'project': serilizator.data})
 
@@ -21,3 +27,18 @@ class ProjectListAPI(APIView):
         projects = Project.objects.filter(user=request.user, is_deleted=False)
         serilizator = self.class_serializer(projects, many=True)
         return Response({'projects': serilizator.data})
+
+
+class ProjectDataAPI(APIView):
+    class_serializer = ProjectSerializer
+
+    def get_object(self, uuid, user):
+        try:
+            return Project.objects.get(uuid=uuid, user=user)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, uuid, *args, **kwargs):
+        project = self.get_object(uuid, request.user)
+        serilizator = self.class_serializer(project, many=False)
+        return Response({'project': serilizator.data})
