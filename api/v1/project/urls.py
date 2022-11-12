37764f9575bdb06386af00a0a0a7718cc48f7987
Retspen <anatoliy.guskov@gmail.com>
2022-11-12 12:30:32 +0200

    Added project default URL

--- api/v1/project/urls.py
+++ api/v1/project/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from project.views import ProjectListAPI
+from project.views import ProjectDefaultAPI, ProjectListAPI
 
 urlpatterns = [
+    re_path(r"default/?$", ProjectDefaultAPI.as_view(), name="project_default_api"),
     re_path(r"$", ProjectListAPI.as_view(), name="project_list_api"),
 ]
 
--- project/views.py
+++ project/views.py
@@ -4,10 +4,20 @@ from rest_framework.response import Response
 from .models import Project
 from .serializers import ProjectSerializer
 
+
+class ProjectDefaultAPI(APIView):
+    class_serializer = ProjectSerializer
+
+    def get(self, request, *args, **kwargs):
+        projects = Project.objects.get(user=request.user, is_default=True, is_deleted=False)
+        serilizator = self.class_serializer(projects, many=False)
+        return Response({'project': serilizator.data})
+
+
 class ProjectListAPI(APIView):
     class_serializer = ProjectSerializer
 
     def get(self, request, *args, **kwargs):
-        projects = Project.objects.filter(user=request.user)
+        projects = Project.objects.filter(user=request.user, is_deleted=False)
         serilizator = self.class_serializer(projects, many=True)
         return Response({'projects': serilizator.data})
