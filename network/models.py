6fb0770d43066691c968d9ea4c7fff1d4b7ca974
Retspen <anatoliy.guskov@gmail.com>
2022-11-22 22:43:40 +0200

    Added network model

---
+++ network/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Network(models.Model):
+    PUBLIC = "public"
+    PRIVATE = "private"
+    COMPUTE = "compute"
+    TYPE_CHOICES = (
+        (PUBLIC, "Public"),
+        (PRIVATE, "Private"),
+        (COMPUTE, "Compute"),
+    )
+    IPv4 = 4
+    IPv6 = 6
+    VERSION_CHOICES = (
+        (IPv4, "IPv4"),
+        (IPv6, "IPv6"),
+    )
+    cidr = models.GenericIPAddressField()
+    netmask = models.GenericIPAddressField()
+    gateway = models.GenericIPAddressField()
+    dns1 = models.GenericIPAddressField()
+    dns2 = models.GenericIPAddressField()
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES, default=PUBLIC)
+    version = models.IntegerField(choices=VERSION_CHOICES, default=IPv4)
+    region = models.ForeignKey("region.Region", models.PROTECT)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Aggregate"
+        verbose_name_plural = "Aggregates"
+        ordering = ["-id"]
+        
+    def __unicode__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.updated = timezone.now()
+        super(Compute(), self).save(*args, **kwargs)
+
+
+class IPAddress(models.Model):
+    network = models.ForeignKey(Network, models.PROTECT)
+    address = models.GenericIPAddressField()
+    netmask = models.GenericIPAddressField()
+
+    class Meta:
+        verbose_name = "IP Address"
+        verbose_name_plural = "IP Addresses"
+        ordering = ["-id"]
+
+    def __unicode__(self):
+        return self.address
--- webvirtcloud/settings/base.py
+++ webvirtcloud/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS += [
     "compute",
     "keypair",
     "project",
+    "network",
     "singlepage",
 ]
 
