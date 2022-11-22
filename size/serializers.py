99194cbdcd1687963259d59323463fa5c1c99b83
Retspen <anatoliy.guskov@gmail.com>
2022-11-22 12:28:43 +0200

    Added transfer to sizes

--- size/serializers.py
+++ size/serializers.py
@@ -9,7 +9,10 @@ class SizeSerializer(serializers.ModelSerializer):
     available = serializers.BooleanField(source="is_active")
     price_hourly = serializers.DecimalField(source='price', max_digits=10, decimal_places=6)
     price_monthly = serializers.SerializerMethodField()
+    disk = serializers.SerializerMethodField()
+    memory = serializers.SerializerMethodField()
     regions = serializers.SerializerMethodField()
+    transfer = serializers.SerializerMethodField()
 
     class Meta:
         model = Size
@@ -18,12 +21,22 @@ class SizeSerializer(serializers.ModelSerializer):
             "memory",
             "vcpu",
             "disk",
+            "transfer",
             "description",
             "available",
             "price_hourly",
             "price_monthly",
             "regions",
         )
+
+    def get_disk(self, obj):
+        return obj.disk // 1073741824
+
+    def get_memory(self, obj):
+        return obj.memory // 1048576
+    
+    def get_transfer(self, obj):
+        return obj.transfer / 1099511627776
 
     def get_price_monthly(self, obj):
         return int(round(obj.price * 730, 0))
