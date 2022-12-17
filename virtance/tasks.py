d9d6278ad190880c90d3dc51ac2d3938ff832905
Retspen <anatoliy.guskov@gmail.com>
2022-12-17 11:57:33 +0200

    Added base template for create virtance

--- virtance/tasks.py
+++ virtance/tasks.py
@@ -1,4 +1,5 @@
 import requests
+from django.conf import settings
 from webvirtcloud.celery import app
 
 from compute.helper import assign_free_compute, WebVirtCompute
@@ -33,8 +34,61 @@ def create_virtance(virtance_id, password):
         ipv4_private = IPAddress.objects.get(network__type=Network.PRIVATE, virtance=virtance)
 
     if compute and ipv4_public and ipv4_compute and ipv4_private:
-        images = []
-        network = []
+        images = [
+            {
+                "name": virtance.name,
+                "size": virtance.size.disk,
+                "url": settings.IMAGE_URL + "/" + virtance.image.slug + ".qcow2",
+                "md5sum": virtance.image.md5sum,
+                "primary": True
+            }
+        ]
+        network = [
+            {
+                "v4": {
+                    "public": {
+                        "primary": {
+                            "address": ipv4_public.address,
+                            "gateway": ipv4_public.network.gateway,
+                            "netmask": ipv4_public.network.netmask,
+                            "dns1": ipv4_public.network.dns1,
+                            "dns2": ipv4_public.network.dns2
+                            
+                        },
+                        "secondary": {
+                            "address": ipv4_compute.address,
+                            "gateway": ipv4_compute.network.gateway,
+                            "netmask": ipv4_compute.network.netmask
+                        }
+                    },
+                    "private": {
+                        "primary": {
+                            "address": ipv4_private.address,
+                            "gateway": ipv4_private.network.gateway,
+                            "netmask": ipv4_private.network.netmask
+                            
+                        },
+                        "secondary": {
+                            "address": "",
+                            "gateway": "",
+                            "netmask": ""
+                        }
+                    }
+                },
+                "v6": {
+                    "public": {
+                        "primary": {
+                            "address": "",
+                            "gateway": "",
+                            "prefix": "",
+                            "dns1": "",
+                            "dns2": ""
+                            
+                        }
+                    }
+                }
+            }
+        ]
         keypairs = []
         wvcomp = WebVirtCompute(compute.token, compute.hostname)
         wvcomp.create_virtance(
