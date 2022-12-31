4136a06c09153c72db64b9307c4eb75cd23ad935
Retspen <anatoliy.guskov@gmail.com>
2022-12-31 15:17:01 +0200

    Added ARCH for compute

--- compute/models.py
+++ compute/models.py
@@ -4,8 +4,15 @@ from django.utils import timezone
 
 
 class Compute(models.Model):
+    X86_64 = "x86_64"
+    AARCH64 = "aarch64"
+    ARCH_CHOICES = [
+        (X86_64, "X64"),
+        (AARCH64, "ARM64")
+    ]
     uuid = models.UUIDField(unique=True, editable=False, default=uuid4)
     name = models.CharField(max_length=255)
+    arch = models.CharField(max_length=50, choices=ARCH_CHOICES, default=X86_64)
     description = models.TextField()
     hostname = models.CharField(max_length=255)
     token = models.CharField(max_length=255)
