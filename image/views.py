f0d17b1fbd8c14f2821583bb45159ef18bebbdec
Retspen <anatoliy.guskov@gmail.com>
2023-01-04 18:51:19 +0200

    Refactoring

--- image/views.py
+++ image/views.py
@@ -10,11 +10,11 @@ class ImageListAPI(APIView):
     class_serializer = ImageSerializer
 
     def get_queryset(self):
+        queryset = Image.objects.filter(is_deleted=False)
         image_type = self.request.query_params.get('type', None)
-        images = Image.objects.filter(is_deleted=False)
         if image_type is not None:
-            images = images.filter(type=image_type)
-        return images
+            queryset = queryset.filter(type=image_type)
+        return queryset
 
     def get(self, request, *args, **kwargs):
         serilizator = self.class_serializer(self.get_queryset(), many=True)
