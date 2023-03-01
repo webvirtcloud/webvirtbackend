bc9b63387e9560af925731a6f2865d930e7c0820
Retspen <anatoliy.guskov@gmail.com>
2023-03-01 17:25:13 +0200

    Fixed URLs

--- api/v1/virtance/urls.py
+++ api/v1/virtance/urls.py
@@ -4,6 +4,6 @@ from virtance.views import VirtanceListAPI, VirtanceDataAPI, VirtanceActionAPI
 
 urlpatterns = [
     re_path(r"$", VirtanceListAPI.as_view(), name="virtance_list_api"),
-    re_path(r"(?P<id>\d+)/$", VirtanceDataAPI.as_view(), name="virtance_data_api"),
-    re_path(r"(?P<id>\d+)/action/$", VirtanceActionAPI.as_view(), name="virtance_action_api"),
+    re_path(r"(?P<id>\d+)/?$", VirtanceDataAPI.as_view(), name="virtance_data_api"),
+    re_path(r"(?P<id>\d+)/action/?$", VirtanceActionAPI.as_view(), name="virtance_action_api"),
 ]
