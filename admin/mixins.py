76161cfb4001eb77e9ecc696d11b6b6d8fb6f284
Retspen <anatoliy.guskov@gmail.com>
2023-03-04 19:59:02 +0200

    Added check and redirect for non auth user

--- admin/mixins.py
+++ admin/mixins.py
@@ -12,11 +12,11 @@ class LoginRequiredMixin:
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             if not request.user.is_active:
-                return redirect(reverse_lazy("admin_sing_out"))
+                return redirect(reverse_lazy("admin_sign_out"))
 
         if self.admin_required:
             if not request.user.is_admin:
                 messages.error(request, "You don't have permission to access this page.")
-                return redirect(reverse_lazy("admin_sing_out"))
+                return redirect(reverse_lazy("admin_sign_out"))
 
         return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
--- admin/views.py
+++ admin/views.py
@@ -1,4 +1,6 @@
 from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import AdminAuthForm
@@ -10,10 +12,18 @@ class AdminSingInView(LoginView):
     template_name = "admin/sign_in.html"
     redirect_authenticated_user=True
 
+    def form_valid(self, form):
+        user = form.get_user()
+        if user.is_admin is True:
+            login(self.request, user)
+            return HttpResponseRedirect(self.get_success_url())
+        return HttpResponseRedirect(reverse_lazy("admin_sign_in"))        
+
 
 class AdminSingOutView(LoginRequiredMixin, LogoutView):
     template_name = "admin/sign_out.html"
 
 
-class AdminIndexView(TemplateView):
+class AdminIndexView(LoginRequiredMixin, TemplateView):
+    admin_required = True
     template_name = "admin/index.html"
