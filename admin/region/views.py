d1520d4fce9a45ac35b4a096abf438b02bd227c3
Retspen <anatoliy.guskov@gmail.com>
2023-03-05 15:34:12 +0200

    Added CRUD for region

--- admin/region/views.py
+++ admin/region/views.py
@@ -1,18 +1,23 @@
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, UpdateView, DeleteView
 from .forms import FormRegion
 from region.models import Region
+from admin.mixins import LoginRequiredMixin
 
-
-class AdminRegionIndexView(TemplateView):
+class AdminRegionIndexView(LoginRequiredMixin, TemplateView):
     template_name = 'admin/region/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['regions'] = Region.objects.filter(is_deleted=False)
+        return context
 
-class AdminRegionCreateView(FormView):
+
+class AdminRegionCreateView(LoginRequiredMixin, FormView):
     template_name = 'admin/region/create.html'
     form_class = FormRegion
-    success_url = '/admin/region/'
+    success_url = reverse_lazy('admin_region_index')
 
     def form_valid(self, form):
         form.save()
@@ -22,3 +27,26 @@ class AdminRegionCreateView(FormView):
         context = super().get_context_data(**kwargs)
         context['regions'] = Region.objects.filter(is_deleted=False)
         return context
+
+
+class AdminRegionUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = 'admin/region/update.html'
+    template_name_suffix = "_form"
+    model = Region
+    success_url = reverse_lazy('admin_region_index')
+    fields = [
+        "name",
+        "slug",
+        "description",
+        "is_active",
+    ]
+
+class AdminRegionDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = 'admin/region/delete.html'
+    model = Region
+    success_url = reverse_lazy('admin_region_index')
+
+    def delete(self, request, *args, **kwargs):
+        region = self.get_object()
+        region.delete()
+        return super(self).delete(request, *args, **kwargs)
--- region/models.py
+++ region/models.py
@@ -6,8 +6,8 @@ class Region(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
-    is_active = models.BooleanField(default=False)
-    is_deleted = models.BooleanField(default=False)
+    is_active = models.BooleanField("Active", default=False)
+    is_deleted = models.BooleanField("Deleted", default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
     deleted = models.DateTimeField(blank=True, null=True)
@@ -20,6 +20,12 @@ class Region(models.Model):
     def save(self, *args, **kwargs):
         self.updated = timezone.now()
         super(Region, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.is_active = False
+        self.is_deleted = True
+        self.deleted = timezone.now()
+        self.save()
 
     def __unicode__(self):
         return self.name
