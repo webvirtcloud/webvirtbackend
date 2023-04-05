837bc19a71215c373627f7bc6299797967f56ba2
Retspen <anatoliy.guskov@gmail.com>
2023-04-05 10:47:06 +0300

    Refactoring task

--- virtance/tasks.py
+++ virtance/tasks.py
@@ -15,6 +15,11 @@ from keypair.models import KeyPairVirtance
 from .models import Virtance
 
 
+def wvcomp_conn(virtance_id):
+    virtance = Virtance.objects.get(id=virtance_id)
+    return WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
+
+
 @app.task
 def create_virtance(virtance_id, password):
     keypairs = []
@@ -94,12 +99,10 @@ def create_virtance(virtance_id, password):
 @app.task
 def rebuild_virtance(virtance_id):
     keypairs = []
-    compute = None
     ipv4_public = None
     ipv4_compute = None
     ipv4_private = None
     virtance = Virtance.objects.get(id=virtance_id)
-    compute = Compute.objects.get(virtance=virtance)
     ipv4_compute = IPAddress.objects.get(network__type=Network.COMPUTE, virtance=virtance)
     ipv4_public = IPAddress.objects.get(network__type=Network.PUBLIC, virtance=virtance)
     ipv4_private = IPAddress.objects.get(network__type=Network.PRIVATE, virtance=virtance)
@@ -143,7 +146,7 @@ def rebuild_virtance(virtance_id):
         "v6": None
     }
    
-    wvcomp = WebVirtCompute(compute.token, compute.hostname)
+    wvcomp = WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
     wvcomp.rebuild_virtance(
         virtance.id,
         virtance.name,
@@ -156,57 +159,57 @@ def rebuild_virtance(virtance_id):
 
 @app.task
 def action_virtance(virtance_id, action):
-    virtance = Virtance.objects.get(id=virtance_id)
-    wvcomp = WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
-    wvcomp.action_virtance(virtance.id, action)
+    wvcomp = wvcomp_conn(virtance_id)
+    wvcomp.action_virtance(virtance_id, action)        
+
 
 @app.task
 def resize_virtance(virtance_id, vcpu, memory, disk_size):
-    virtance = Virtance.objects.get(id=virtance_id)
-    wvcomp = WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
-    wvcomp.resize_virtance(virtance.id, vcpu, memory, disk_size)
+    wvcomp = wvcomp_conn(virtance_id)
+    wvcomp.resize_virtance(virtance_id, vcpu, memory, disk_size)
+
 
 @app.task
 def snapshot_virtance(virtance_id, image_name):
-    virtance = Virtance.objects.get(id=virtance_id)
-    wvcomp = WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
-    wvcomp.snapshot_virtance(virtance.id, image_name)
+    wvcomp = wvcomp_conn(virtance_id)
+    wvcomp.snapshot_virtance(virtance_id, image_name)
+
 
 @app.task
 def restore_virtance(virtance_id, image_name, disk_size):
-    virtance = Virtance.objects.get(id=virtance_id)
-    wvcomp = WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
-    wvcomp.restore_virtance(virtance.id, image_name, disk_size)
+    wvcomp = wvcomp_conn(virtance_id)
+    wvcomp.restore_virtance(virtance_id, image_name, disk_size)
+
 
 @app.task
 def reset_password_virtance(virtance_id, password):
+    wvcomp = wvcomp_conn(virtance_id)
     password_hash = sha512_crypt.encrypt(password, salt=uuid4().hex[0:8], rounds=5000)
-    virtance = Virtance.objects.get(id=virtance_id)
-    wvcomp = WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
-    wvcomp.reset_password_virtance(virtance.id, password_hash)
+    wvcomp.reset_password_virtance(virtance_id, password_hash)
+
 
 @app.task
 def enable_recovery_mode_virtance(virtance_id):
-    virtance = Virtance.objects.get(id=virtance_id)
-    wvcomp = WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
-    res = wvcomp.get_virtance_media(virtance.id)
+    wvcomp = wvcomp_conn(virtance_id)
+    res = wvcomp.get_virtance_media(virtance_id)
     if isinstance(res, list):
-        res = wvcomp.mount_virtance_media(virtance.id, res[0].get("dev"), settings.RECOVERY_ISO_NAME)
+        res = wvcomp.mount_virtance_media(virtance_id, res[0].get("dev"), settings.RECOVERY_ISO_NAME)
+
 
 @app.task
 def disable_recovery_mode_virtance(virtance_id):
-    virtance = Virtance.objects.get(id=virtance_id)
-    wvcomp = WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
-    res = wvcomp.get_virtance_media(virtance.id)
+    wvcomp = wvcomp_conn(virtance_id)
+    res = wvcomp.get_virtance_media(virtance_id)
     if isinstance(res, list) and res[0].get("path") is not None:
-        wvcomp.umount_virtance_media(virtance.id, res[0].get("dev"), res[0].get("path"))
+        wvcomp.umount_virtance_media(virtance_id, res[0].get("dev"), res[0].get("path"))
+
 
 @app.task
 def delete_virtance(virtance_id):
-    virtance = Virtance.objects.get(id=virtance_id)
-    wvcomp = WebVirtCompute(virtance.compute.token, virtance.compute.hostname)
-    error = wvcomp.delete_virtance(virtance.id)
-    if not error:
+    wvcomp = wvcomp_conn(virtance_id)
+    res = wvcomp.delete_virtance(virtance_id)
+    if not res:
+        virtance = Virtance.objects.get(id=virtance_id)
         ipaddresse = IPAddress.objects.filter(virtance=virtance)
         ipaddresse.delete()
         virtance.delete()
