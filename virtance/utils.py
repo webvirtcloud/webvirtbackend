33cee31f726b0c879848dd34f585f2d6bb798f6e
Retspen <anatoliy.guskov@gmail.com>
2023-04-22 17:30:04 +0300

    Fixed typo

--- virtance/utils.py
+++ virtance/utils.py
@@ -16,5 +16,5 @@ def make_vnc_hash(vnc_password):
     return b64encode((f"{make_passwd(6)}{vnc_password}{make_passwd(8)}").encode())
 
 
-def virtance_error(id, message, event=None):
-    VirtanceError.objects.create(id=id, message=message, event=event)
+def virtance_error(virtance_id, message, event=None):
+    VirtanceError.objects.create(virtance_id=virtance_id, message=message, event=event)
