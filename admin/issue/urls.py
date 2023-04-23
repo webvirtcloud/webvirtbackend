1a508f6e5445b604ebad42e85f5f6671006fb324
Retspen <anatoliy.guskov@gmail.com>
2023-04-23 21:44:19 +0300

    Added virtance error list view

--- admin/issue/urls.py
+++ admin/issue/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from .views import AdminIssueIndexView
+from .views import AdminIssueIndexView, AdminIssueVirtanceView
 
 
 urlpatterns = [
     re_path("$", AdminIssueIndexView.as_view(), name="admin_issue_index"),
+    re_path("virtance/?$", AdminIssueVirtanceView.as_view(), name="admin_issue_virtance"),
 ]
--- admin/issue/views.py
+++ admin/issue/views.py
@@ -1,3 +1,6 @@
+from django.utils import timezone
+
+from virtance.models import VirtanceError
 from admin.mixins import AdminTemplateView
 
 
@@ -6,4 +9,14 @@ class AdminIssueIndexView(AdminTemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        return context
+
+
+class AdminIssueVirtanceView(AdminTemplateView):
+    template_name = 'admin/issue/virtance.html'
+
+    def get_context_data(self, **kwargs):
+        thirty_days_ago = (timezone.now() - timezone.timedelta(days=30))
+        context = super().get_context_data(**kwargs)
+        context["virtace_errors"] = VirtanceError.objects.filter(created__gte=thirty_days_ago)
         return context
