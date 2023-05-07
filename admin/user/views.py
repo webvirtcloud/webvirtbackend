d344543070d7a59ff638f7255b2355b909c14060
Retspen <anatoliy.guskov@gmail.com>
2023-05-07 22:58:29 +0300

    Updated HTML templates

--- admin/user/views.py
+++ admin/user/views.py
@@ -1,9 +1,12 @@
 from admin.mixins import AdminTemplateView
 
+from account.models import User
+
 
 class AdminUserIndexView(AdminTemplateView):
     template_name = 'admin/user/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['users'] = User.objects.filter(is_admin=False)
         return context
