b8a0a514b4e19c5167c7a974c58f5f3ccacbc958
Retspen <anatoliy.guskov@gmail.com>
2023-07-12 19:57:06 +0300

    Added user page to admin side

--- admin/user/urls.py
+++ admin/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from .views import AdminUserIndexView
+from .views import AdminUserIndexView, AdminUserDataView
 
 
 urlpatterns = [
     re_path("$", AdminUserIndexView.as_view(), name="admin_user_index"),
+    re_path(r"^(?P<pk>\d+)/$", AdminUserDataView.as_view(), name="admin_user_data"),
 ]
--- admin/user/views.py
+++ admin/user/views.py
@@ -1,6 +1,7 @@
-from admin.mixins import AdminTemplateView
+from django.shortcuts import get_object_or_404
 
 from account.models import User
+from admin.mixins import AdminTemplateView
 
 
 class AdminUserIndexView(AdminTemplateView):
@@ -9,4 +10,16 @@ class AdminUserIndexView(AdminTemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['users'] = User.objects.filter(is_admin=False)
+        return context
+
+
+class AdminUserDataView(AdminTemplateView):
+    template_name = 'admin/user/user.html'
+
+    def get_object(self):
+        return get_object_or_404(User, pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.get_object()
         return context
