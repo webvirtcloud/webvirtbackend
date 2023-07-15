1d4d7f4dfb2a7df005a37b56f38f6f41b97e8a4c
Retspen <anatoliy.guskov@gmail.com>
2023-07-15 19:25:19 +0300

    Added images error page to admin side

--- admin/issue/urls.py
+++ admin/issue/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
-from .views import AdminIssueIndexView, AdminIssueVirtanceView
+from .views import AdminIssueIndexView, AdminIssueVirtanceView, AdminIssueImageView
 
 
 urlpatterns = [
     re_path("$", AdminIssueIndexView.as_view(), name="admin_issue_index"),
+    re_path("image/?$", AdminIssueImageView.as_view(), name="admin_issue_image"),
     re_path("virtance/?$", AdminIssueVirtanceView.as_view(), name="admin_issue_virtance"),
 ]
--- admin/issue/views.py
+++ admin/issue/views.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 
+from image.models import ImageError
 from virtance.models import VirtanceError
 from admin.mixins import AdminTemplateView
 
@@ -20,5 +21,17 @@ class AdminIssueVirtanceView(AdminTemplateView):
         context = super().get_context_data(**kwargs)
         context["virtances_errors"] = VirtanceError.objects.filter(
             virtance__is_deleted=False, created__gte=thirty_days_ago
+        )
+        return context
+
+
+class AdminIssueImageView(AdminTemplateView):
+    template_name = 'admin/issue/image.html'
+
+    def get_context_data(self, **kwargs):
+        thirty_days_ago = timezone.now() - timezone.timedelta(days=30)
+        context = super().get_context_data(**kwargs)
+        context["images_errors"] = ImageError.objects.filter(
+            image__is_deleted=False, created__gte=thirty_days_ago
         )
         return context
