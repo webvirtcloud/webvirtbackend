65cf428ee67f9420f070e48b567a5337b9e4a69a
Retspen <anatoliy.guskov@gmail.com>
2023-07-15 11:30:48 +0300

    Refactoring delete image task

--- image/tasks.py
+++ image/tasks.py
@@ -10,25 +10,7 @@ from .utils import image_error
 def image_delete(image_id):
     image = Image.objects.get(pk=image_id)
     
-    if image.type == Image.BACKUP:
-        wvcomp = WebVirtCompute(
-            image.source.compute.token, 
-            image.source.compute.hostname
-        )
-        res = wvcomp.get_storages()
-        if res.get("detail") is None:
-            for storage in res.get("storages"):
-                res = wvcomp.get_storage(storage.get("name"))  
-                if res.get("detail") is None:
-                    for vol in res.get("volumes"):
-                        if vol.get("name") == image.file_name:
-                            res = wvcomp.delete_storage_volume(storage.get("name"), vol.get("name"))
-                            if res.get("detail") is None:
-                                image.delete()
-        if res.get("detail"):
-            image_error(image.id, res.get("detail"), "delete_backup")
-
-    if image.type == Image.SNAPSHOT:
+    if image.type == Image.SNAPSHOT or image.type == Image.BACKUP:
         for region in image.regions.all():
             computes = Compute.objects.filter(region=region, is_active=True, is_deleted=False).order_by("?")
             for compute in computes:
@@ -45,4 +27,7 @@ def image_delete(image_id):
                                         image.regions.remove(region)
                                         break
                 if res.get("detail"):
-                    image_error(image.id, res.get("detail"), "delete_snapshot")
+                    image_error(image.id, res.get("detail"), f"delete_{image.type}_image")
+
+        if image.regions.count() == 0:
+            image.delete()
