5d98e92bbb337b5cff2bacedf0ee52402ce42c81
Retspen <anatoliy.guskov@gmail.com>
2023-07-15 20:20:10 +0300

    Added virtance history func

--- virtance/utils.py
+++ virtance/utils.py
@@ -2,7 +2,7 @@ from random import choice
 from base64 import b64encode
 from string import digits, ascii_letters
 
-from .models import VirtanceError
+from .models import VirtanceError, VirtanceHistory
 
 
 def make_passwd(length=8):
@@ -18,5 +18,10 @@ def make_vnc_hash(vnc_password, prefix_length=6, suffix_length=8):
     )
 
 
+def virtance_history(virtance_id, user_id, message, event=None):
+    VirtanceHistory.objects.create(virtance_id=virtance_id, user_id=user_id, message=message, event=event)
+
+
 def virtance_error(virtance_id, message, event=None):
     VirtanceError.objects.create(virtance_id=virtance_id, message=message, event=event)
+
