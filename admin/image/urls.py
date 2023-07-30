3f4ab2c7995329c130683a9415463bc91ef93d73
Retspen <anatoliy.guskov@gmail.com>
2023-07-30 21:24:04 +0300

    Added reset event for images

--- admin/image/urls.py
+++ admin/image/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
-from .views import AdminImageIndexView, AdminImageDataView
+from .views import AdminImageIndexView, AdminImageDataView, AdminImageResetEventAction
 
 
 urlpatterns = [
     re_path("$", AdminImageIndexView.as_view(), name="admin_image_index"),
     re_path(r"^(?P<pk>\d+)/$", AdminImageDataView.as_view(), name="admin_image_data"),
+    re_path(r"^(?P<pk>\d+)/reset_event/$", AdminImageResetEventAction.as_view(), name="admin_image_reset_event"),
 ]
--- admin/image/views.py
+++ admin/image/views.py
@@ -1,8 +1,9 @@
 from django.db.models import Q
-from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.shortcuts import redirect, get_object_or_404
 
 from image.models import Image
-from admin.mixins import AdminTemplateView
+from admin.mixins import AdminView, AdminTemplateView
 
 
 class AdminImageIndexView(AdminTemplateView):
@@ -26,3 +27,16 @@ class AdminImageDataView(AdminTemplateView):
         context = super().get_context_data(**kwargs)
         context['image'] = self.get_object()
         return context
+
+
+class AdminImageResetEventAction(AdminView):
+
+    def get_object(self):
+        return get_object_or_404(
+            Image, Q(type=Image.BACKUP) | Q(type=Image.SNAPSHOT), pk=self.kwargs['pk'], is_deleted=False
+        )
+        
+    def post(self, request, *args, **kwargs):
+        image = self.get_object()
+        image.reset_event()
+        return redirect(reverse("admin_image_data", args=[kwargs.get("pk")]))
