f301211422974cba7aee4a7f1147a25abf1b2c10
Retspen <anatoliy.guskov@gmail.com>
2023-09-15 19:33:15 +0300

    Added message for updating network if it is already using

--- admin/network/views.py
+++ admin/network/views.py
@@ -1,5 +1,7 @@
 from ipaddress import ip_network
+from django.contrib import messages
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from crispy_forms.helper import FormHelper
 
 from .forms import FormNetwork
@@ -8,18 +10,18 @@ from admin.mixins import AdminTemplateView, AdminFormView, AdminUpdateView, Admi
 
 
 class AdminNetworkIndexView(AdminTemplateView):
-    template_name = 'admin/network/index.html'
+    template_name = "admin/network/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['networks'] = Network.objects.filter(is_deleted=False)
+        context["networks"] = Network.objects.filter(is_deleted=False)
         return context
 
 
 class AdminNetworkCreateView(AdminFormView):
-    template_name = 'admin/network/create.html'
+    template_name = "admin/network/create.html"
     form_class = FormNetwork
-    success_url = reverse_lazy('admin_network_index')
+    success_url = reverse_lazy("admin_network_index")
 
     def form_valid(self, form):
         form.save()
@@ -27,16 +29,22 @@ class AdminNetworkCreateView(AdminFormView):
 
 
 class AdminNetworkUpdateView(AdminUpdateView):
-    template_name = 'admin/network/update.html'
+    template_name = "admin/network/update.html"
     template_name_suffix = "_form"
     model = Network
-    success_url = reverse_lazy('admin_network_index')
+    success_url = reverse_lazy("admin_network_index")
     fields = ["cidr", "netmask", "dns1", "dns2", "is_active"]
 
     def __init__(self, *args, **kwargs):
         super(AdminNetworkUpdateView, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_tag = False
+
+    def dispatch(self, request, *args, **kwargs):
+        if IPAddress.objects.filter(network_id=kwargs.get("pk")).exists():
+            messages.error(self.request, "Network has assigned IP addresses and cannot be updated")
+            return redirect(reverse_lazy("admin_network_index"))
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         cidr = form.cleaned_data.get("cidr")
@@ -54,9 +62,9 @@ class AdminNetworkUpdateView(AdminUpdateView):
 
 
 class AdminNetworkDeleteView(AdminDeleteView):
-    template_name = 'admin/network/delete.html'
+    template_name = "admin/network/delete.html"
     model = Network
-    success_url = reverse_lazy('admin_network_index')
+    success_url = reverse_lazy("admin_network_index")
 
     def delete(self, request, *args, **kwargs):
         region = self.get_object()
@@ -68,6 +76,12 @@ class AdminNetworkDeleteView(AdminDeleteView):
         self.helper = FormHelper()
         self.helper.form_tag = False
 
+    def dispatch(self, request, *args, **kwargs):
+        if IPAddress.objects.filter(network_id=kwargs.get('pk')).exists():
+            messages.error(self.request, "Network has assigned IP addresses and cannot be deleted")
+            return redirect(reverse_lazy("admin_network_index"))
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super(AdminNetworkDeleteView, self).get_context_data(**kwargs)
         context['helper'] = self.helper
@@ -75,11 +89,11 @@ class AdminNetworkDeleteView(AdminDeleteView):
 
 
 class AdminNetworkListView(AdminTemplateView):
-    template_name = 'admin/network/list.html'
+    template_name = "admin/network/list.html"
 
     def get_context_data(self, **kwargs):
         context = super(AdminNetworkListView, self).get_context_data(**kwargs)
-        network = Network.objects.filter(pk=self.kwargs.get("pk")).first()
+        network = Network.objects.filter(pk=kwargs.get("pk")).first()
         ip_addresses = IPAddress.objects.filter(network=network)
         context["network"] = network
         context["ip_addresses"] = ip_addresses
--- network/helper.py
+++ network/helper.py
@@ -13,11 +13,7 @@ def assign_free_ipv4_compute(virtance_id):
     virtance = Virtance.objects.get(id=virtance_id)
     virtances = Virtance.objects.filter(compute=virtance.compute, is_deleted=False)
     network = Network.objects.get(
-        region=virtance.region,
-        version=Network.IPv4, 
-        type=Network.COMPUTE, 
-        is_active=True, 
-        is_deleted=False
+        region=virtance.region, version=Network.IPv4, type=Network.COMPUTE, is_active=True, is_deleted=False
     )
     assigned_ipv4_compute = IPAddress.objects.filter(network=network, virtance__in=virtances)
     ipaddrs = list(IPv4Network(f"{network.cidr}/{network.netmask}"))[SUBNET_RANGE]
@@ -31,11 +27,7 @@ def assign_free_ipv4_compute(virtance_id):
 def assign_free_ipv4_public(virtance_id):
     virtance = Virtance.objects.get(id=virtance_id)
     networks = Network.objects.filter(
-        region=virtance.region, 
-        version=Network.IPv4, 
-        type=Network.PUBLIC, 
-        is_active=True, 
-        is_deleted=False
+        region=virtance.region, version=Network.IPv4, type=Network.PUBLIC, is_active=True, is_deleted=False
     )
     for net in networks:
         ipaddrs = list(IPv4Network(f"{net.cidr}/{net.netmask}"))[SUBNET_RANGE]
@@ -49,11 +41,7 @@ def assign_free_ipv4_public(virtance_id):
 def assign_free_ipv4_private(virtance_id):
     virtance = Virtance.objects.get(id=virtance_id)
     networks = Network.objects.filter(
-        region=virtance.region,
-        version=Network.IPv4, 
-        type=Network.PRIVATE, 
-        is_active=True, 
-        is_deleted=False
+        region=virtance.region, version=Network.IPv4, type=Network.PRIVATE, is_active=True, is_deleted=False
     )
     for net in networks:
         ipaddrs = list(IPv4Network(f"{net.cidr}/{net.netmask}"))[SUBNET_RANGE]
@@ -67,19 +55,15 @@ def assign_free_ipv4_private(virtance_id):
 def assign_free_ipv6_public(virtance_id):
     virtance = Virtance.objects.get(id=virtance_id)
     networks = Network.objects.filter(
-        region=virtance.region,
-        version=Network.IPv6, 
-        type=Network.PUBLIC,
-        is_active=True,
-        is_deleted=False
+        region=virtance.region, version=Network.IPv6, type=Network.PUBLIC, is_active=True, is_deleted=False
     )
     for net in networks:
         step = 16
-        nums = 2 ** step
+        nums = 2**step
         subnet = IPv6Network(f"{net.cidr}/{net.netmask}")
         start = int(subnet.network_address) + step
         end = int(subnet.broadcast_address) + 1
-        limit = nums if nums < end else end - 1 # Check if limit is greater than end
+        limit = nums if nums < end else end - 1  # Check if limit is greater than end
         for i in random.sample(range(start, end, step), k=limit):
             if not IPAddress.objects.filter(network=net, address=str(ip_address(i))).exists():
                 ipaddr = IPAddress.objects.create(network=net, address=str(ip_address(i)), virtance=virtance)
