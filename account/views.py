0eeaf8e67a90e693af872a7b01d90526156f8fdf
Retspen <anatoliy.guskov@gmail.com>
2023-09-16 15:25:01 +0300

    Added resend confirm email

--- account/views.py
+++ account/views.py
@@ -3,10 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.conf import settings
 
 from webvirtcloud.views import error_message_response
 from project.models import Project
 from .models import User, Token
+from .tasks import email_confirm_register
 from .serializers import (
     RegisterSerializer,
     AuthTokenSerializer,
@@ -73,19 +75,32 @@ class ResetPasswordHash(APIView):
         return error_message_response("User not found or hash is invalid.")
 
 
-class VerifyEmail(APIView):
+
+class VerifyResendEmail(APIView):
     permission_classes = (IsAuthenticated,)
 
-    def get(self, request, hash, *args, **kwargs):
-        if User.objects.filter(hash=hash, is_email_verified=False, is_active=True).exists():
-            user = User.objects.get(hash=hash)
+    def post(self, request, *args, **kwargs):
+        if request.user.is_email_verified:
+            return error_message_response("Account already verified.")
+        request.user.update_hash()
+        confirm_url = f"{settings.CLIENT_URL}/account/verify_email/{request.user.hash}/"
+        email_confirm_register.delay(confirm_url, request.user.email)
+        return Response(status=status.HTTP_200_OK)
+
+
+class VerifyHashEmail(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        if User.objects.filter(hash=kwargs.get("hash"), is_email_verified=False, is_active=True).exists():
+            user = User.objects.get(hash=kwargs.get("hash"))
             user.is_email_verified = True
             user.update_hash()
             user.save()
 
             user_name = user.email.split("@")[0]
             project_name = f"{user_name.capitalize()}'s project"
-            project = Project.objects.create(name=project_name, user=user, is_default=True)
+            Project.objects.create(name=project_name, user=user, is_default=True)
 
             return Response(status=status.HTTP_200_OK)
         return error_message_response("Invalid token or email already verified.")
