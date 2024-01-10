1c63a79088b7608757d009ef1399cd50ce71f388
Retspen <anatoliy.guskov@gmail.com>
2024-01-10 22:13:35 +0200

    Added user edit func to admin side

--- admin/user/urls.py
+++ admin/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import re_path
-from .views import AdminUserIndexView, AdminUserCreateView, AdminUserDataView
+from .views import AdminUserIndexView, AdminUserCreateView, AdminUserUpdateView, AdminUserDataView
 
 
 urlpatterns = [
     re_path(r"$", AdminUserIndexView.as_view(), name="admin_user_index"),
     re_path(r"create/?$", AdminUserCreateView.as_view(), name="admin_user_create"),
     re_path(r"^(?P<pk>\d+)/$", AdminUserDataView.as_view(), name="admin_user_data"),
+    re_path(r"^(?P<pk>\d+)/update/?$", AdminUserUpdateView.as_view(), name="admin_user_update"),
 ]
--- admin/user/views.py
+++ admin/user/views.py
@@ -1,9 +1,11 @@
+from django.urls import reverse
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+from crispy_forms.helper import FormHelper
 
 from account.models import User
 from admin.user.forms import FormUser
-from admin.mixins import AdminTemplateView, AdminFormView
+from admin.mixins import AdminTemplateView, AdminFormView, AdminUpdateView
 
 
 class AdminUserIndexView(AdminTemplateView):
@@ -23,6 +25,30 @@ class AdminUserCreateView(AdminFormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+class AdminUserUpdateView(AdminUpdateView):
+    template_name = "admin/user/update.html"
+    template_name_suffix = "_form"
+    model = User
+    fields = ["first_name", "last_name", "is_active"]
+
+    def __init__(self, *args, **kwargs):
+        super(AdminUserUpdateView, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(AdminUserUpdateView, self).get_context_data(**kwargs)
+        context["helper"] = self.helper
+        return context
+    
+    def get_success_url(self):
+        return reverse("admin_user_data", args=[self.kwargs.get("pk")])
 
 
 class AdminUserDataView(AdminTemplateView):
