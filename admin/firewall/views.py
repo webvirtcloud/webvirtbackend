da5460dd5ec18482eb1b39d8c3aa69273e1e36c0
Retspen <anatoliy.guskov@gmail.com>
2024-01-27 23:08:00 +0200

    Added info about num rules and virtances in firewall

--- admin/firewall/views.py
+++ admin/firewall/views.py
@@ -1,5 +1,5 @@
-from firewall.models import Firewall
 from admin.mixins import AdminTemplateView
+from firewall.models import Firewall, Rule, FirewallVirtance, FirewallError
 
 
 class AdminFirewallIndexView(AdminTemplateView):
@@ -7,5 +7,9 @@ class AdminFirewallIndexView(AdminTemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["firewalls"] = Firewall.objects.filter(is_deleted=False)
+        firewalls = Firewall.objects.filter(is_deleted=False)
+        for firewall in firewalls:
+            firewall.num_rule = len(Rule.objects.filter(firewall=firewall))
+            firewall.num_virtance = len(FirewallVirtance.objects.filter(firewall=firewall))
+        context["firewalls"] = firewalls
         return context
