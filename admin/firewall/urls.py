3be9a6a1f22999e4d51e835841f3d20ee003232d
Retspen <anatoliy.guskov@gmail.com>
2024-02-18 20:03:32 +0200

    Added firewall detail page on admin side

--- admin/firewall/urls.py
+++ admin/firewall/urls.py
@@ -1,6 +1,10 @@
 from django.urls import re_path
-from .views import AdminFirewallIndexView
+from .views import AdminFirewallIndexView, AdminFirewallDataView, AdminFirewallResetEventAction
 
 urlpatterns = [
     re_path(r"$", AdminFirewallIndexView.as_view(), name="admin_firewall_index"),
+    re_path(r"^(?P<pk>\d+)/$", AdminFirewallDataView.as_view(), name="admin_firewall_data"),
+    re_path(
+        r"^(?P<pk>\d+)/reset_event/$", AdminFirewallResetEventAction.as_view(), name="admin_firewall_reset_event"
+    ),
 ]
--- admin/firewall/views.py
+++ admin/firewall/views.py
@@ -1,4 +1,7 @@
-from admin.mixins import AdminTemplateView
+from django.urls import reverse
+from django.shortcuts import redirect, get_object_or_404
+
+from admin.mixins import AdminView, AdminTemplateView
 from firewall.models import Firewall, Rule, FirewallVirtance, FirewallError
 
 
@@ -13,3 +16,29 @@ class AdminFirewallIndexView(AdminTemplateView):
             firewall.num_virtance = len(FirewallVirtance.objects.filter(firewall=firewall))
         context["firewalls"] = firewalls
         return context
+
+
+class AdminFirewallDataView(AdminTemplateView):
+    template_name = "admin/firewall/firewall.html"
+
+    def get_object(self):
+        return get_object_or_404(Firewall, pk=self.kwargs["pk"], is_deleted=False)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        firewall = self.get_object()
+        context["firewall"] = firewall
+        context["rules"] = Rule.objects.filter(firewall=firewall)
+        context["virtances"] = FirewallVirtance.objects.filter(firewall=firewall)
+        context["firewall_errors"] = FirewallError.objects.filter(firewall=firewall)
+        return context
+
+
+class AdminFirewallResetEventAction(AdminView):
+    def get_object(self):
+        return get_object_or_404(Firewall, pk=self.kwargs["pk"], is_deleted=False)
+
+    def post(self, request, *args, **kwargs):
+        firewall = self.get_object()
+        firewall.reset_event()
+        return redirect(reverse("admin_firewall_data", args=[kwargs.get("pk")]))
