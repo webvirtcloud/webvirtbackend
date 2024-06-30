87d4cb3f08869711765fae7716d5038eb7353fbe
Retspen <anatoliy.guskov@gmail.com>
2024-06-30 17:12:07 +0300

    Added view for load balancers

--- api/v1/load_balancer/urls.py
+++ api/v1/load_balancer/urls.py
@@ -1,8 +1,12 @@
 from django.urls import re_path
 
-from lbaas.views import LBaaSListAPI
+from lbaas.views import LBaaSListAPI, LBaaSDataAPI, LBaaSVirtancesAPI, LBaaSForwardRulesAPI
 
 urlpatterns = [
     re_path(r"$", LBaaSListAPI.as_view(), name="lbaas_list_api"),
-    re_path(r"(?P<uuid>[0-9a-f-]+)/?$", LBaaSListAPI.as_view(), name="lbaas_data_api"),
+    re_path(r"(?P<uuid>[0-9a-f-]+)/?$", LBaaSDataAPI.as_view(), name="lbaas_data_api"),
+    re_path(r"(?P<uuid>[0-9a-f-]+)/virtances/?$", LBaaSVirtancesAPI.as_view(), name="lbaas_virtance_api"),
+    re_path(
+        r"(?P<uuid>[0-9a-f-]+)/forwarding_rules/?$", LBaaSForwardRulesAPI.as_view(), name="lbaas_forward_rules_api"
+    ),
 ]
--- lbaas/views.py
+++ lbaas/views.py
@@ -1,7 +1,9 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
+from webvirtcloud.views import error_message_response
 from .models import LBaaS
 from .serializers import LBaaSSerializer
 
@@ -22,3 +24,66 @@ class LBaaSListAPI(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"load_balancer": serializer.data}, status=status.HTTP_201_CREATED)
+
+
+class LBaaSDataAPI(APIView):
+    class_serializer = LBaaSSerializer
+
+    def get_object(self):
+        return get_object_or_404(LBaaS, uuid=self.kwargs.get("uuid"), user=self.request.user, is_deleted=False)
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.class_serializer(self.get_object(), many=False)
+        return Response({"load_balancer": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        lbaas = self.get_object()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class LBaaSVirtancesAPI(APIView):
+    class_serializer = LBaaSSerializer
+
+    def get_object(self):
+        return get_object_or_404(LBaaS, uuid=self.kwargs.get("uuid"), user=self.request.user, is_deleted=False)
+
+    def post(self, request, *args, **kwargs):
+        lbaas = self.get_object()
+
+        if lbaas.event is not None:
+            return error_message_response("The load balancer already has event.")
+
+        serializer = self.class_serializer(lbaas, many=False)
+        return Response({"load_balancer": serializer.data})
+    
+    def delete(self, request, *args, **kwargs):
+        lbaas = self.get_object()
+
+        if lbaas.event is not None:
+            return error_message_response("The load balancer already has event.")
+    
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class LBaaSForwardRulesAPI(APIView):
+    class_serializer = LBaaSSerializer
+
+    def get_object(self):
+        return get_object_or_404(LBaaS, uuid=self.kwargs.get("uuid"), user=self.request.user, is_deleted=False)
+
+    def post(self, request, *args, **kwargs):
+        lbaas = self.get_object()
+
+        if lbaas.event is not None:
+            return error_message_response("The load balancer already has event.")
+
+        serializer = self.class_serializer(self.get_object(), many=False)
+        return Response({"load_balancer": serializer.data})
+    
+    def delete(self, request, *args, **kwargs):
+        lbaas = self.get_object()
+
+        if lbaas.event is not None:
+            return error_message_response("The load balancer already has event.")
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
