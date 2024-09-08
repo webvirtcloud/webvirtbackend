1602ac5b59e8b3352b7e7a8c3cdcf027a2cbf12b
Retspen <anatoliy.guskov@gmail.com>
2024-09-08 19:37:07 +0300

    Added docstring to LBaaS

--- lbaas/views.py
+++ lbaas/views.py
@@ -25,10 +25,112 @@ class LBaaSListAPI(APIView):
         return queryset
 
     def get(self, request, *args, **kwargs):
+        """
+        List All Load Balancers
+        ---
+        """
         serilizator = self.class_serializer(self.get_queryset(), many=True)
         return Response({"load_balancers": serilizator.data})
 
     def post(self, request, *args, **kwargs):
+        """
+        Create a New Load Balancer
+        ---
+            parameters:
+                - name: name
+                  description: Load Balancer Name
+                  required: true
+                  type: string
+
+                - name: region
+                  description: Region
+                  required: true
+                  type: string
+                
+                - name: virtance_ids
+                  description: Virtances to be Attached to Load Balanced
+                  required: true
+                  type: array
+                  items:
+                    type: integer
+
+                - name: redirect_http_to_https
+                  description: Redirect HTTP to HTTPS 
+                  required: false
+                  type: boolean
+
+                - name: sticky_session
+                  description: Sticky Session
+                  required: fal
+                  type: object
+                  properties:
+                    cookie_ttl_seconds:
+                      description: Cookie TTL
+                      required: true
+                      type: string
+                    cookie_name:
+                      description: Cookie Name
+                      required: true
+                      type: string
+
+                - name: health_check
+                  description: Health Check
+                  required: true
+                  type: object
+                  properties:
+                    protocol:
+                      description: Protocol (HTTP or TCP)
+                      required: true
+                      type: string
+                    port:
+                      description: Port for Health Check
+                      required: true
+                      type: integer
+                    path:
+                      description: HTTP Path (required for HTTP only)
+                      required: false
+                      type: string
+                    check_interval_seconds:
+                      description: Interval
+                      required: true
+                      type: integer
+                    response_timeout_seconds:
+                      description: Check Timeout
+                      required: true
+                      type: integer
+                    healthy_threshold:
+                      description: Healthy Threshold
+                      required: true
+                      type: integer
+                    unhealthy_threshold:
+                      description: Unhealthy Threshold
+                      required: true
+                      type: integer
+                
+                - name: forwarding_rules
+                  description: Forward Rules
+                  required: true
+                  type: array
+                  items:
+                    type: object
+                    properties:
+                      entry_port:
+                        description: Entry Port (1-65535)
+                        required: true
+                        type: integer
+                      entry_protocol:
+                        description: Entry Protocol (TCP, UDP, HTTP, HTTPS, HTTP2 and HTTP3)
+                        required: true
+                        type: integer
+                      target_port:
+                        description: Target Port (1-65535)
+                        required: true
+                        type: integer
+                      target_protocol:
+                        description: Target Protocol (TCP, UDP, HTTP, HTTPS, HTTP2 and HTTP3)
+                        required: true
+                        type: integer
+        """
         serializer = self.class_serializer(data=request.data, context={"user": request.user})
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -42,14 +144,31 @@ class LBaaSDataAPI(APIView):
         return get_object_or_404(LBaaS, uuid=self.kwargs.get("uuid"), user=self.request.user, is_deleted=False)
 
     def put(self, request, *args, **kwargs):
+        """
+        Update The Existing Load Balancer
+        ---
+            parameters:
+                - name: name
+                  description: Load Balancer Name
+                  required: true
+                  type: string
+        """
         serializer = self.class_serializer(self.get_object(), many=False)
         return Response({"load_balancer": serializer.data})
 
     def get(self, request, *args, **kwargs):
+        """
+        Retrieve Existing Load Balancer
+        ---
+        """
         serializer = self.class_serializer(self.get_object(), many=False)
         return Response({"load_balancer": serializer.data})
 
     def delete(self, request, *args, **kwargs):
+        """
+        Delete The Load Balancer
+        ---
+        """
         lbaas = self.get_object()
 
         if lbaas.event is not None:
@@ -64,6 +183,37 @@ class LBaaSForwardRulesAPI(APIView):
         return get_object_or_404(LBaaS, uuid=self.kwargs.get("uuid"), user=self.request.user, is_deleted=False)
 
     def post(self, request, *args, **kwargs):
+        """
+        Add Forward Rule to The Load Balancer
+        ---
+            parameters:
+                name: forwarding_rules
+                description: Forward Rules
+                required: true
+                type: array
+                items:
+                    type: object
+                    properties:
+                        entry_port:
+                          description: Entry Port (1-65535)
+                          required: true
+                          type: integer
+
+                        entry_protocol:
+                          description: Entry Protocol (TCP, UDP, HTTP, HTTPS, HTTP2 and HTTP3)
+                          required: true
+                          type: integer
+
+                        target_port:
+                          description: Target Port (1-65535)
+                          required: true
+                          type: integer
+
+                        target_protocol:
+                          description: Target Protocol (TCP, UDP, HTTP, HTTPS, HTTP2 and HTTP3)
+                          required: true
+                          type: integer
+        """
         lbaas = self.get_object()
 
         if lbaas.event is not None:
@@ -75,6 +225,37 @@ class LBaaSForwardRulesAPI(APIView):
         return Response(serializer.data)
 
     def delete(self, request, *args, **kwargs):
+        """
+        Delete Forward Rule from The Load Balancer
+        ---
+            parameters:
+                name: forwarding_rules
+                description: Forward Rules
+                required: true
+                type: array
+                items:
+                    type: object
+                    properties:
+                        entry_port:
+                          description: Entry Port (1-65535)
+                          required: true
+                          type: integer
+
+                        entry_protocol:
+                          description: Entry Protocol (TCP, UDP, HTTP, HTTPS, HTTP2 and HTTP3)
+                          required: true
+                          type: integer
+
+                        target_port:
+                          description: Target Port (1-65535)
+                          required: true
+                          type: integer
+
+                        target_protocol:
+                          description: Target Protocol (TCP, UDP, HTTP, HTTPS, HTTP2 and HTTP3)
+                          required: true
+                          type: integer        
+        """
         lbaas = self.get_object()
 
         if lbaas.event is not None:
@@ -91,6 +272,10 @@ class LBaaSVirtancesAPI(APIView):
         return get_object_or_404(LBaaS, uuid=self.kwargs.get("uuid"), user=self.request.user, is_deleted=False)
 
     def get(self, request, *args, **kwargs):
+        """
+        List All Virtances Attached to The Load Balancer
+        ---
+        """
         virtance_ids = LBaaSVirtance.objects.filter(lbaas=self.get_object(), is_deleted=False).values_list(
             "virtance_id", flat=True
         )
@@ -99,6 +284,17 @@ class LBaaSVirtancesAPI(APIView):
         return Response({"virtances": serilizator.data})
 
     def post(self, request, *args, **kwargs):
+        """
+        Attach Virtance to The Load Balancer
+        ---
+            parameters:
+                - name: virtance_ids
+                  description: Virtance ID
+                  required: true
+                  type: array
+                    items:
+                        type: integer
+        """
         lbaas = self.get_object()
 
         if lbaas.event is not None:
@@ -110,6 +306,17 @@ class LBaaSVirtancesAPI(APIView):
         return Response(serializer.data)
 
     def delete(self, request, *args, **kwargs):
+        """
+        Detach Virtance from The Load Balancer
+        ---
+            parameters:
+                - name: virtance_ids
+                  description: Virtance ID
+                  required: true
+                  type: array
+                    items:
+                        type: integer
+        """
         lbaas = self.get_object()
 
         if lbaas.event is not None:
