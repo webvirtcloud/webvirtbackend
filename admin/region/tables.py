cdc28dd70bf5f09c8706509bb7f836d730b53e15
Retspen <anatoliy.guskov@gmail.com>
2025-02-02 16:08:25 +0200

    Black refformatter

--- admin/region/tables.py
+++ admin/region/tables.py
@@ -5,15 +5,13 @@ from region.models import Region
 
 class RegionHTMxTable(tables.Table):
     active = tables.TemplateColumn(
-        template_name="django_tables2/is_active_column.html",
-        verbose_name="Active", 
-        accessor="is_active"
+        template_name="django_tables2/is_active_column.html", verbose_name="Active", accessor="is_active"
     )
     action = tables.TemplateColumn(
         template_name="admin/region/action_column.html",
         verbose_name="Action",
         attrs={"th": {"data-type": "action"}, "td": {"data-type": "action"}},
-        orderable=False
+        orderable=False,
     )
 
     class Meta:
